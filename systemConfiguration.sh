7a33728e4d19d139a8e81df7badc09e465d3958f
Victor_Piccoli <victorvp2008@hotmail.com>
2018-02-08 10:57:02 -0200

    Issues with permissions patch

--- systemConfiguration.sh
+++ systemConfiguration.sh
@@ -33,6 +33,7 @@ sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+sudo chmod -R 777 "$HOME/.nvm" 
 nvm install 8.0.0
 nvm install 6.0.0
 echo
