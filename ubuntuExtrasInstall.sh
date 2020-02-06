45ad19fb87e164b19bab34c6f9328eaf5747e735
Lypzis <victorvp2008@hotmail.com>
2020-02-06 10:55:15 -0300

    Adding new usefull script

---
+++ ubuntuExtrasInstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt install -y libdvdnav4 libdvdread4 gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly libdvd-pkg
+sudo apt install -y ubuntu-restricted-extras
+## In case of errors,execute next line 
+#sudo dpkg-reconfigure libdvd-pkg
